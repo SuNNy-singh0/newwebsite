437cf149d884b9b64000c8f5553ef1a913f09ca5
Tonystark7862 <sunnysingh29122001@gmail.com>
2025-08-04 21:14:58 +0530

    fix error

--- src/Compoents/Homepage/Home.jsx
+++ src/Compoents/Homepage/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Banner from './Banner'
 import Navbar from './Navbar'
 import TMFeatures from './TmFeatures'
-import HowWeWorkReplica from './HowWeWorkReplica'
+
 import BookingSteps from './BookingSteps'
-import Partner from './Partner'
+
 import TownmanorBanner from './TownmanorBanner'
 import StayMemories from './StayMemories'
 
